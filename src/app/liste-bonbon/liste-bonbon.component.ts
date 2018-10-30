cbdf021f7ece4874b286cf67250b8524f774dc67
Léa <l.delacotte@yahoo.fr>
2018-10-30 15:50:58 +0100

    First commit

--- src/app/liste-bonbon/liste-bonbon.component.ts
+++ src/app/liste-bonbon/liste-bonbon.component.ts
@@ -7,7 +7,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListeBonbonComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+
+  export const Bonbons: Bonbon[] = [
+    { nom: 'Fraise tagada', marque : 'Haribo', texture :'mou', gout:'doux', image:'https://static.openfoodfacts.org/images/products/310/322/000/9079/front_fr.12.200.jpg' },
+    { nom: 'Arlequin', marque: 'Lutti', texture :'dur', gout:'doux', image:'https://static.openfoodfacts.org/images/products/311/674/001/7332/front_fr.28.200.jpg' },
+    { nom: 'Smarties', marque: 'Nestlé', texture :'dur', gout:'doux', saveur : 'chocolat', image:'https://static.openfoodfacts.org/images/products/40345192/front_fr.45.200.jpg' },
+    { nom: 'Têtes Brûlées', marque: 'Verquin', texture :'dur', gout:'acide', image:'https://static.openfoodfacts.org/images/products/10360634/front_fr.4.200.jpg'  },
+    { nom: 'Flexi-fizz', marque: 'Lutti', texture :'mou', gout:'acide', image:'https://static.openfoodfacts.org/images/products/541/005/718/7039/front_fr.6.200.jpg'  },
+    { nom: 'Surffizz', marque: 'Lutti', texture :'mou', gout:'acide', image:'https://static.openfoodfacts.org/images/products/311/674/003/3608/front_fr.4.200.jpg'  },
+    { nom: 'Chamallows', marque: 'Haribo', texture :'mou', gout:'doux', image:'https://static.openfoodfacts.org/images/products/310/322/000/9512/front_fr.37.200.jpg'  },
+    { nom: 'Dragibus', marque: 'Haribo', texture :'dur', gout:'doux', image:'https://static.openfoodfacts.org/images/products/310/322/002/5338/front_fr.29.200.jpg'  },
+    { nom: 'Bubblizz', marque: 'Lutti', texture :'mou', gout:'acide', image:'https://static.openfoodfacts.org/images/products/541/005/716/9110/front_fr.16.200.jpg'  },
+    { nom: 'Chupa chups', marque: 'Chupa chups', texture :'dur', gout:'doux', image:'https://static.openfoodfacts.org/images/products/541/005/716/9110/front_fr.16.200.jpg'  },
+    { nom: 'Werthers Original', marque: 'Werthers', texture :'dur', gout:'doux', saveur : 'caramel' image:'https://static.openfoodfacts.org/images/products/40144283/front_fr.36.200.jpg'  },
+    { nom: 'Schoko-Bons', marque: 'Kinder', texture :'mou', gout:'doux', saveur : 'chocolat', image:'https://ii.worldmarket.com/fcgi-bin/iipsrv.fcgi?FIF=/images/worldmarket/source/23185_XXX_v1.tif&wid=650&cvt=jpeg'  },
+    
+    
+
+  ];
+}
 
   ngOnInit() {
   }
