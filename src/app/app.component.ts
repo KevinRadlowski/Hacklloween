d4c97d37c9e360967b60b0b9eb01e36fda61885c
chelgoui shems <shems.chelgoui@gmail.com>
2018-10-30 20:16:23 +0100

    allright

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -8,6 +8,33 @@ import { Component } from '@angular/core';
 })
 
 export class AppComponent {
-  
+  isPlayer = true;
+  pseudo: string ;
+  myLat: number;
+  myLng: number;
+  constructor(private router: Router) {}
+  submitSubForm() {
+    switch (this.isPlayer) {
+      case true:
+        this.router.navigate(['joueur/' + this.pseudo]);
+        break;
+
+      case false:
+        this.router.navigate(['maison/' + this.pseudo]);
+        break;
+    }
+  }
+
+  displayLocationInfo = (position) => {
+    this.myLng = position.coords.longitude;
+    this.myLat = position.coords.latitude;
+  }
+
+  ngonInit() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(this.displayLocationInfo);
+    }
+  }
+
 }
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -17,10 +17,19 @@ import { AgmCoreModule } from '@agm/core';
 import { MapMarkersComponent } from './map-markers/map-markers.component';
 
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-
+import { Routes, RouterModule} from '@angular/router';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatListModule } from '@angular/material/list';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+const appRoutes: Routes = [
+  {path: '**', redirectTo: 'form', pathMatch: 'full'},
+  {path: 'form', component: FormComponent},
+  {path: 'joueur/id:', component: JoueurComponent},
+  {path: 'maison/id:', component: MaisonComponent},
+  ];
+
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -35,13 +44,16 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     QuestionComponent,
     QuestionCreationComponent,
     ListeBonbonComponent,
-    MapMarkersComponent
+    MapMarkersComponent,
+    FormJoueurComponent,
+    FormMaisonComponent
   ],
   entryComponents: [
     PanierComponent
   ],
   imports: [
     BrowserModule,
+    RouterModule.forRoot(appRoutes),
     AgmCoreModule.forRoot({
       apiKey: 'AIzaSyBNiGHB5tcAsl9ilspi0aqb093Nlvdy5L8'
     }),
