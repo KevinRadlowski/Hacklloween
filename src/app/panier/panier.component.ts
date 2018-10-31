7aba78512e26da55dbf69af1e2ec6a48f06d660f
Emeric <emeric555@gmail.com>
2018-10-31 11:44:00 +0100

    panier fonctionnel

--- src/app/panier/panier.component.ts
+++ src/app/panier/panier.component.ts
@@ -1,18 +1,36 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { NgbActiveModal, NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { BonbonService } from "../service/bonbon.service"
 
 @Component({
   selector: 'app-panier',
   templateUrl: './panier.component.html',
   styleUrls: ['./panier.component.css']
 })
+
+
 export class PanierComponent implements OnInit {
   @Input() name;
 
-  constructor(public activeModal: NgbActiveModal) {}
+  bonbonDispo= [];
+
+  constructor(private bonbonservice: BonbonService,
+              public activeModal: NgbActiveModal) {}
+              
 
 
-  ngOnInit() {
-  }
+              ngOnInit() {
+                let notrandom:number;
+                let random:number;
+                let i= 0;
+                while ( i < 10 ){
+                  random = Math.floor(Math.random() * Math.floor(10));
+                  if( random != notrandom){
+                    notrandom = random;
+                    this.bonbonDispo.push(this.bonbonservice.Bonbons[random]);
+                    i++;
+                  }
+                }
+              }
 
 }
